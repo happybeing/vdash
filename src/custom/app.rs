fbff842074d03957d28df17c63adbe9e966f4d31
Mark <git@happybeing.com>
2020-08-24 14:44:28 +0100

    Format on save reformatting (app.rs)

--- src/custom/app.rs
+++ src/custom/app.rs
@@ -1,17 +1,16 @@
 ///! Application logic
 ///!
 ///! Edit src/custom/app.rs to create a customised fork of logtail-dash
-
 use linemux::MuxedLines;
 use std::collections::HashMap;
 
+use std::fs::File;
+use std::io::{Error, ErrorKind, Write};
 use structopt::StructOpt;
-use std::fs::{File};
-use std::io::{Write, Error, ErrorKind};
-use tempfile::{NamedTempFile};
+use tempfile::NamedTempFile;
 
-use crate::shared::util::{StatefulList};  
-use crate::custom::opt::{Opt};
+use crate::custom::opt::Opt;
+use crate::shared::util::StatefulList;
 
 pub struct App {
   pub opt: Opt,
@@ -26,14 +25,15 @@ impl App {
 
     if opt.files.is_empty() {
       println!("{}: no logfile(s) specified.", Opt::clap().get_name());
-      println!("Try '{} --help' for more information.", Opt::clap().get_name());
-      return Err(Error::new(ErrorKind::Other,"missing logfiles"));
+      println!(
+        "Try '{} --help' for more information.",
+        Opt::clap().get_name()
+      );
+      return Err(Error::new(ErrorKind::Other, "missing logfiles"));
     }
-  
     let mut dash_state = DashState::new();
     let mut monitors: HashMap<String, LogMonitor> = HashMap::new();
     let mut logfiles = MuxedLines::new()?;
-  
     let mut parser_output: Option<tempfile::NamedTempFile> = if opt.debug_parser {
       dash_state.main_view = DashViewMain::DashVertical;
       opt.files = opt.files[0..1].to_vec();
@@ -42,8 +42,9 @@ impl App {
       let path_str = path.to_str().unwrap();
       opt.files.push(String::from(path_str));
       Some(named_file)
-    } else { None };
-  
+    } else {
+      None
+    };
     println!("Loading {} files...", opt.files.len());
     for f in &opt.files {
       println!("file: {}", f);
@@ -53,27 +54,30 @@ impl App {
           Some(named_file) => {
             monitor.parser_logfile = Some(named_file);
             parser_output = None;
-          },
+          }
           None => {}
         }
       }
-  
       if opt.ignore_existing {
-          monitors.insert(f.to_string(), monitor);
+        monitors.insert(f.to_string(), monitor);
       } else {
-          match monitor.load_logfile() {
-          Ok(()) => {monitors.insert(f.to_string(), monitor);},
+        match monitor.load_logfile() {
+          Ok(()) => {
+            monitors.insert(f.to_string(), monitor);
+          }
           Err(e) => {
             println!("...failed: {}", e);
             return Err(e);
-          },
+          }
         }
       }
       match logfiles.add_file(&f).await {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(e) => {
           println!("ERROR: {}", e);
-          println!("Note: it is ok for the file not to exist, but the file's parent directory must exist.");
+          println!(
+            "Note: it is ok for the file not to exist, but the file's parent directory must exist."
+          );
           return Err(e);
         }
       }
@@ -91,9 +95,8 @@ impl App {
 pub struct LogMonitor {
   pub index: usize,
   pub content: StatefulList<String>,
-  pub logfile:  String,
+  pub logfile: String,
   pub parser_logfile: Option<NamedTempFile>,
-  
   max_content: usize, // Limit number of lines in content
 }
 
@@ -118,14 +121,14 @@ impl LogMonitor {
     let f = File::open(self.logfile.to_string());
     let f = match f {
       Ok(file) => file,
-      Err(_e) => return Ok(()),  // It's ok for a logfile not to exist yet
+      Err(_e) => return Ok(()), // It's ok for a logfile not to exist yet
     };
 
     let f = BufReader::new(f);
 
     for line in f.lines() {
-        let line = line.expect("Unable to read line");
-        self.process_line(&line);
+      let line = line.expect("Unable to read line");
+      self.process_line(&line);
     }
 
     Ok(())
@@ -134,15 +137,15 @@ impl LogMonitor {
   pub fn process_line(&mut self, text: &str) -> Result<(), std::io::Error> {
     // TODO parse and update metrics
 
-    // Activated for first monitor with --debug-parser 
+    // Activated for first monitor with --debug-parser
     match &self.parser_logfile {
       Some(f) => {
         use std::io::Seek;
         let mut file = f.reopen()?;
         file.seek(std::io::SeekFrom::End(0))?;
         writeln!(file, "{}", text.len())
-      },
-      None => Ok(())
+      }
+      None => Ok(()),
     };
 
     // Show in TUI
@@ -152,7 +155,10 @@ impl LogMonitor {
   pub fn append_to_content(&mut self, text: &str) -> Result<(), std::io::Error> {
     self.content.items.push(text.to_string());
     if self.content.items.len() > self.max_content {
-      self.content.items = self.content.items.split_off(self.content.items.len() - self.max_content);
+      self.content.items = self
+        .content
+        .items
+        .split_off(self.content.items.len() - self.max_content);
     }
     Ok(())
   }
@@ -160,7 +166,10 @@ impl LogMonitor {
   fn _reset_metrics(&mut self) {}
 }
 
-pub enum DashViewMain {DashHorizontal, DashVertical}
+pub enum DashViewMain {
+  DashHorizontal,
+  DashVertical,
+}
 
 pub struct DashState {
   pub main_view: DashViewMain,
@@ -170,9 +179,9 @@ pub struct DashState {
 }
 
 impl DashState {
-  pub fn new() -> DashState { 
+  pub fn new() -> DashState {
     DashState {
-      main_view: DashViewMain::DashHorizontal, 
+      main_view: DashViewMain::DashHorizontal,
       dash_vertical: DashVertical::new(),
     }
   }
@@ -183,7 +192,7 @@ pub struct DashVertical {
 }
 
 impl DashVertical {
-  pub fn new() -> Self { 
-    DashVertical { active_view: 0, }
+  pub fn new() -> Self {
+    DashVertical { active_view: 0 }
   }
 }
