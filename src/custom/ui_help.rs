09959633537ff952ed5e47a309122f1f7449dd7b
Mark <git@happybeing.com>
2024-07-16 11:33:56 +0100

    Restore 'q' to quit to the built in help page

--- src/custom/ui_help.rs
+++ src/custom/ui_help.rs
@@ -1,8 +1,8 @@
 ///! Terminal based interface and dashboard
 ///!
 use super::app::{DashState, HELP_WINDOW_NAME};
-use crate::custom::ui::{ push_subheading, push_text, push_blank, push_multiline_text};
 use crate::custom::opt::{get_app_name, get_app_version};
+use crate::custom::ui::{push_blank, push_multiline_text, push_subheading, push_text};
 
 use ratatui::{
 	layout::Rect,
@@ -10,11 +10,7 @@ use ratatui::{
 	Frame,
 };
 
-
-pub fn draw_help_dash(
-	f: &mut Frame,
-	dash_state: &mut DashState,
-) {
+pub fn draw_help_dash(f: &mut Frame, dash_state: &mut DashState) {
 	draw_help_window(f, f.size(), dash_state);
 }
 
@@ -22,23 +18,33 @@ pub fn draw_help_window(f: &mut Frame, area: Rect, dash_state: &mut DashState) {
 	let mut items = Vec::<ListItem>::new();
 
 	push_blank(&mut items);
-	push_text(&mut items, &String::from("    For vdash command usage:"), None);
+	push_text(
+		&mut items,
+		&String::from("    For vdash command usage:"),
+		None,
+	);
 	push_text(&mut items, &String::from("        vdash --help"), None);
 
 	push_blank(&mut items);
-	push_blank(&mut items);
 	push_subheading(&mut items, &String::from("    Keyboard Commands"));
-	push_multiline_text(&mut items, "
+	push_multiline_text(
+		&mut items,
+		"
     'n' or 'enter' :   Switch to Node Status where you can cycle through status of each node.\n
     's' or 'enter' :   Switch to Summary of all monitored nodes.\n
     'r'            :   Re-scan any 'glob' paths to add new nodes.\n
     '$'            :   Toggle between nanos and a currency (if rate specified on the command line).
 
-    'h' or '?'     :   Shows this help. Press 'n' or 's' to exit help.");
+	'q'            :   Quit vdash.
+    'h' or '?'     :   Shows this help. Press 'n' or 's' to exit help.",
+	);
 
 	push_blank(&mut items);
 	push_blank(&mut items);
-	push_subheading(&mut items, &String::from("    Node Status: selecting a node"));
+	push_subheading(
+		&mut items,
+		&String::from("    Node Status: selecting a node"),
+	);
 	push_blank(&mut items);
 
 	push_text(&mut items, &String::from("    Use right arrow and left arrow to cycle forward and backwards through multiple monitored nodes."), None);
@@ -69,11 +75,16 @@ pub fn draw_help_window(f: &mut Frame, area: Rect, dash_state: &mut DashState) {
 		push_text(&mut items, &String::from("    'g' for debug window"), None);
 	}
 
-	let help_title_text =format!("{} v{} - {}", get_app_name(), get_app_version(), String::from(HELP_WINDOW_NAME));
+	let help_title_text = format!(
+		"{} v{} - {}",
+		get_app_name(),
+		get_app_version(),
+		String::from(HELP_WINDOW_NAME)
+	);
 	let help_widget = List::new(items).block(
 		Block::default()
 			.borders(Borders::ALL)
 			.title(help_title_text),
-		);
+	);
 	f.render_stateful_widget(help_widget, area, &mut dash_state.help_status.state);
 }
